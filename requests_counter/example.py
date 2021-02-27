c7c202410631c8a8a7ae80b2240e11c840495a7f
alfarruggia@gmail.com <alfarruggia@gmail.com>
2021-02-27 12:39:39 +0000

    all

--- requests_counter/example.py
+++ requests_counter/example.py
@@ -10,16 +10,21 @@ app = FastAPI()
 cl = ReqCounter("redis://localhost")
 
 # 3. populate the Object with a list of tuple (key, max_value)
-asyncio.create_task(cl.setup([("my-api-key-test", 10)]))
+asyncio.create_task(cl.setup_api_key([("my-api-key-test", 10)]))
+asyncio.create_task(cl.setup_origin(["origin_1","origin_2"]))
 
 
 # 4. Declare a function to inject to Depends module. It will decrease the max_value for each request. It will raise a 429 HTTPException when max_value is 0.
-async def check_key(apiKey: str = Header(None)):
+async def check_key(apiKey: str = Header(None), Origin:str = Header(None)):
+    print(Origin)
     res = await cl.decrease(apiKey)
     if res is False:
-        raise HTTPException(429, "Too Many Requests", headers={"Retry-After": "renew subscription"})
-    return apiKey
+        raise HTTPException(400, "User Requests Limit Exceeded")
 
+    if await cl.check_origin(Origin):
+        raise HTTPException(403, "Forbidden", headers={"Retry-After": "renew subscription"})
+    
+    return apiKey
 
 # 5. Inject the check_key function to endpoint
 @app.get("/consume")
