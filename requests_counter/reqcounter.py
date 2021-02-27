45132a21fbeef3c1d7c2c06206f6c2e08e9deef1
alfarruggia@gmail.com <alfarruggia@gmail.com>
2021-02-27 09:24:16 +0000

    add check origin methods

--- requests_counter/reqcounter.py
+++ requests_counter/reqcounter.py
@@ -6,21 +6,24 @@ class ReqCounter:
         self.rds = RedisDB(url)
         self.__origin_internal_key = "rc_list_origin"
 
-    async def setup_api_key(self, values):
-        await self.rds.init_api_key(values)
+    async def setup_api_key(self, tuple_key_threshold):
+        await self.rds.setup()
+        [await self.rds.create_entry(key_value[0], key_value[1]) for key_value in tuple_key_threshold]
     
-    async def setup_origin(self, values):
-        await self.rds.init_origin(self.__origin_internal_key, values)
+    async def setup_origin(self, list_of_origin):
+        await self.rds.setup()
+        await self.rds.create_entry(self.__origin_internal_key, ",".join(list_of_origin))
 
     async def check_origin(self, origin):
-        return origin in await self.status(self.__origin_internal_key)
+        list_of_origin = await self.rds.status(self.__origin_internal_key)
+        return origin in list_of_origin.decode("utf-8").split(",")
 
     async def decrease(self, key):
         value = await self.rds.decrease(key)
-        return True if value > 0 else False
+        return True if int(value) > 0 else False
 
     async def reset(self, key, value):
-        return await self.rds.set_key_value(key, value)
+        return int(await self.rds.set_key_value(key, value))
 
     async def destroy(self, key):
         return await self.rds.destroy(key)
@@ -29,7 +32,10 @@ class ReqCounter:
         return [await self.rds.destroy(key) for key in await self.rds.all_keys()]
 
     async def status(self):
-        return [{"key": key, "value": await self.rds.status(key)} for key in await self.rds.all_keys()]
+        status_dict = dict()
+        for key in await self.rds.all_keys():
+            status_dict[key.decode("utf-8")] = int(await self.rds.status(key))
+        return status_dict
 
     async def close(self):
         return await self.rds.close()
